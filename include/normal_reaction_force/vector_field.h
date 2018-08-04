521aab50cab4ef846cdc2c31fda0fa7a459e5803
matchey <19305109+matchey@users.noreply.github.com>
2018-08-04 21:22:21 +0900

    update

--- include/normal_reaction_force/vector_field.h
+++ include/normal_reaction_force/vector_field.h
@@ -15,22 +15,24 @@
 
 namespace normal_reaction_force{
 
-	template <int _Dims>
 	class VectorField{
 		public:
 		VectorField();
-		VectorField(const double&);
+		VectorField(const int&, const double&);
 		~VectorField();
-		void setCellSize(const double&);
+		// void setCellSize(const double&);
 		void init(const Eigen::Vector2d&);
-		Eigen::Vector2d* operator[](const int&);
+		Eigen::Vector2d& xy2grid();
+		std::vector<Eigen::Vector2d>& operator[](const int&);
 		Eigen::Vector2d& operator()(const int&, const int&);
 		void publish();
 
 		private:
 		int grid_dim; // grid_dimensions [個]
 		double m_per_cell; // cell_size [m]
-		Eigen::Vector2d val[_Dims][_Dims];
+		std::vector< std::vector<Eigen::Vector2d> > val;
+		// std::vector<Eigen::Vector2d> *val;
+		// Eigen::Vector2d **val;
 	};
 
 } // namespace normal_reaction_force
--- src/vector_field.cpp
+++ src/vector_field.cpp
@@ -12,44 +12,45 @@
 
 namespace normal_reaction_force{
 
-	template<int _Dims>
-	VectorField<_Dims>::VectorField()
-		: grid_dim(_Dims), m_per_cell(0.5)
+	VectorField::VectorField()
+		: grid_dim(320), m_per_cell(0.5)
 	{
+		val.resize(320);
+		for(int i=0; i<320; ++i){
+			val[i].resize(320);
+		}
 	}
 
-	template<int _Dims>
-	VectorField<_Dims>::VectorField(const double& m_per_cell_)
-		: grid_dim(_Dims), m_per_cell(m_per_cell_)
+	VectorField::VectorField(const int& grid_dim_, const double& m_per_cell_)
+		: grid_dim(grid_dim_), m_per_cell(m_per_cell_)
 	{
+		val.resize(grid_dim_);
+		for(int i=0; i<grid_dim_; ++i){
+			val[i].resize(grid_dim_);
+		}
 	}
 
-	template<int _Dims>
-	VectorField<_Dims>::~VectorField() {}
+	VectorField::~VectorField() {}
 
-	template<int _Dims>
-	void VectorField<_Dims>::init(const Eigen::Vector2d& vec)
+	void VectorField::init(const Eigen::Vector2d& vec)
 	{
-		for(unsigned i = 0; i < grid_dim; ++i){
-			for(unsigned j = 0; j < grid_dim; ++j){
+		for(int i = 0; i < grid_dim; ++i){
+			for(int j = 0; j < grid_dim; ++j){
 				val[i][j] = vec;
 			}
 		}
 	}
 
-	template<int _Dims>
-	void VectorField<_Dims>::setCellSize(const double& m_per_cell_)
-	{
-		m_per_cell = m_per_cell_;
-	}
+	// void VectorField::setCellSize(const double& m_per_cell_)
+	// {
+	// 	m_per_cell = m_per_cell_;
+	// }
 
-	template<int _Dims>
-	Eigen::Vector2d* VectorField<_Dims>::operator[](const int& i){
+	std::vector<Eigen::Vector2d>& VectorField::operator[](const int& i){
 		return val[i];
 	}
 
-	template<int _Dims>
-	Eigen::Vector2d& VectorField<_Dims>::operator()(const int& i, const int& j){
+	Eigen::Vector2d& VectorField::operator()(const int& i, const int& j){
 		return val[i][j];
 	}
 
